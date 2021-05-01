f28ba79905e028fb543c41e49a68737f2b4772ae
Ivan Maiseyenka <ivan.maiseyenka@gmail.com>
2021-05-01 22:05:54 +0300

    email sender

---
+++ src/main/java/org/example/sweaterApp/config/MvcConfig.java
@@ -0,0 +1,30 @@
+package org.example.sweaterApp.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+/*
+                Сопоставьте URL-путь или шаблон с контроллером представления, чтобы отобразить ответ с помощью настроенный код состояния и просмотр.
+*/
+                registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+/*        Добавляет обработчик ресурсов для обслуживания статических ресурсов.*/
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file://" + uploadPath + "/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
