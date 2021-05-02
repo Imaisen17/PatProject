44c2dee8c9955461cdb6b01b80773db7a7a376eb
Ivan Maiseyenka <ivan.maiseyenka@gmail.com>
2021-05-02 17:35:25 +0300

    validation and fix bags

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,5 +1,5 @@
 insert into user (id, username, password, active)
-    value (1, 'admin', '123', true);
+    value (1, 'admin', '$2a$08$PI/RRR9.GtWYY1ZzT9ppBO/F2QIEmvNi4Lpdrgr0YdKd4gRCIZ392', true);
 
 insert into user_role (user_id, roles)
     value (1, 'USER'), (1, 'ADMIN');
